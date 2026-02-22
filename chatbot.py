43866b059329176d7142d50aba22bd272ce5f7fb
vivek-devda <vivekblz79@gmail.com>
2026-02-22 22:23:10 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -90,14 +90,15 @@ def chatbot():
             break
 
         user_input = preprocess(user_input)
-        #check the pdf first
+
+        # check the PDF first
         pdf_reply = search_pdf_answer(user_input, pdf_text)
 
-if pdf_reply:
-    print("Bot:", pdf_reply)
-else:
-    reply = get_response(user_input, responses)
-    print("Bot:", reply)
+        if pdf_reply:
+            print("Bot:", pdf_reply)
+        else:
+            reply = get_response(user_input, responses)
+            print("Bot:", reply)
 
 # run chatbot
 if __name__ == "__main__":
